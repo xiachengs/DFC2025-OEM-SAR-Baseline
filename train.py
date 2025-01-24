401c72c291db795e8e2537446549f1d5d61e4711
xiacheng <961518354@qq.com>
2025-01-24 21:08:01 +0800

    去掉val

--- train.py
+++ train.py
@@ -21,37 +21,38 @@ class NamedDataParallel(torch.nn.DataParallel):
 def data_loader(args):
     # get all image paths with ".tif" and "/labels/" in the path
     train_pths = [f for f in Path(args.train_data_root).rglob("*.tif")]
-    val_pths = [f for f in Path(args.val_data_root).rglob("*.tif")]
+    # val_pths = [f for f in Path(args.val_data_root).rglob("*.tif")]
     # Shuffle the paths to randomize the selection
     # random.shuffle(img_pths)
     # # split data: 90% training and 10% validation
     # split_idx= int(0.9 * len(img_pths))
     # convert paths to strings (if needed)
     train_pths = [str(f) for f in train_pths]
-    val_pths = [str(f) for f in val_pths]
+    # val_pths = [str(f) for f in val_pths]
     
     # print("Total samples      :", len(img_pths))
     print("Training samples   :", len(train_pths))
-    print("Validation samples :", len(val_pths))
+    # print("Validation samples :", len(val_pths))
 
     trainset = source.dataset.Dataset(train_pths, classes=args.classes, size=args.crop_size, train=True)
-    validset = source.dataset.Dataset(val_pths, classes=args.classes, train=False)
+    # validset = source.dataset.Dataset(val_pths, classes=args.classes, train=False)
     train_loader = DataLoader(trainset, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers)
-    valid_loader = DataLoader(validset, batch_size=args.batch_size, shuffle=False, num_workers=args.num_workers)
+    # valid_loader = DataLoader(validset, batch_size=args.batch_size, shuffle=False, num_workers=args.num_workers)
     
-    return train_loader, valid_loader
+    # return train_loader, valid_loader
+    return train_loader
 
 def train_model(args, model, optimizer, criterion, metric, device):
     # get dataset loaders
-    train_data_loader, val_data_loader = data_loader(args)
+    train_data_loader = data_loader(args)
     
     # create folder to save model
     os.makedirs(args.save_model, exist_ok=True)
     model_name = f"SAR_Pesudo_{model.name}_s{args.seed}_{criterion.name}"
 
-    max_score = 0
+    # max_score = 0
     train_hist = []
-    valid_hist = []
+    # valid_hist = []
     for epoch in range(args.n_epochs):
         print(f"\nEpoch: {epoch + 1}")
 
@@ -64,23 +65,21 @@ def train_model(args, model, optimizer, criterion, metric, device):
             device=device,
         )
 
-        logs_valid = source.runner.valid_epoch(
-            model=model,
-            criterion=criterion,
-            metric=metric,
-            dataloader=val_data_loader,
-            device=device,
-        )
+        # logs_valid = source.runner.valid_epoch(
+        #     model=model,
+        #     criterion=criterion,
+        #     metric=metric,
+        #     dataloader=val_data_loader,
+        #     device=device,
+        # )
 
         train_hist.append(logs_train)
-        valid_hist.append(logs_valid)
-        score = logs_valid[metric.name]
-        
-        if max_score < score:
-            max_score = score
-            torch.save(model.state_dict(), os.path.join(args.save_model, f"{model_name}.pth"))
-            print("Model saved in the folder : ", args.save_model)
-            print("Model name is : ", model_name)
+        # valid_hist.append(logs_valid)
+        # score = logs_valid[metric.name]
+
+        torch.save(model.state_dict(), os.path.join(args.save_model, f"{model_name}.pth"))
+        print("Model saved in the folder : ", args.save_model)
+        print("Model name is : ", model_name)
      
             
 def main(args):
@@ -128,13 +127,13 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Model Training')
     parser.add_argument('--seed', default=0)
     parser.add_argument('--n_epochs', default=50)
-    parser.add_argument('--batch_size', default=8)
-    parser.add_argument('--num_workers', default=8)
+    parser.add_argument('--batch_size', default=1)
+    parser.add_argument('--num_workers', default=4)
     parser.add_argument('--crop_size', default=512)
     parser.add_argument('--learning_rate', default=0.0001)  
     parser.add_argument('--classes', default=[1, 2, 3, 4, 5, 6, 7, 8])
     parser.add_argument('--train_data_root', default="dataset/train/labels")
-    parser.add_argument('--val_data_root', default="dataset/val/labels")
+    # parser.add_argument('--val_data_root', default="dataset/val/sar_images")
     parser.add_argument('--save_model', default="model")
     parser.add_argument('--save_results', default="results")
     args = parser.parse_args()
